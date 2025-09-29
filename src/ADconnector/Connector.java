5d9810bcfe23f1483c6dbe5dab80042260e5fc89
Diego5RG-dev <dgilis@danielcastelao.org>
2025-09-29 09:01:11 +0200

    Connector hecho

---
+++ src/ADconnector/Connector.java
@@ -0,0 +1,17 @@
+package ADconnector;
+import java.sql.*;
+
+public class Connector {
+    public static Connection conexion(){
+        Connection conn = null;
+        try {
+            String url = "jdbc:mysql://10.0.9.114:3306/AD";
+            String usuario = "postgres";
+            String password = "admin";
+            conn = DriverManager.getConnection(url, usuario, password);
+        }catch (Exception e) {
+            System.out.println(e);
+        }
+        return conn;
+    }
+}
